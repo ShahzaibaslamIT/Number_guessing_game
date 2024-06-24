f0ec43855e133788f8f2e0dcc5243ce58f4e461d
ShahzaibaslamIT <aslamshazaib@gmail.com>
2024-06-24 08:49:36 -0700

    Publishing Number guessing project

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWelcome to my number guessing game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const user_input = await inquirer.prompt([
+    {
+        name: "guessedNumber",
+        type: "number",
+        message: "Enter your number(Limit from 1-5):",
+    }
+]);
+if (user_input.guessedNumber === randomNumber) {
+    console.log("Correct Number! You won!");
+}
+else {
+    console.log("Wrong Number!You lost!");
+}
